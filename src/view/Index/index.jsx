04ef06892b3ac11df34bdaa9fc52d3c7e74ef1f4
王刚 <wanggang1@chinapost.com.cn>
2023-02-16 16:18:29 +0800

    移动端测试开始

--- src/view/Index/index.jsx
+++ src/view/Index/index.jsx
@@ -2,32 +2,26 @@ import { View } from "@keithwang/keith-core";
 
 import "./assets/index.scss";
 
-import NavigationBar from "./parts/NavigationBar";
-import QuickLaunchBar from "./parts/QuickLaunchBar";
-
 class Index extends View {
   constructor(option) {
     super(option);
   }
 
-  handleGoRoute(item) {
-    console.log(item);
+  handleDemo(item) {
+    alert("ok");
     // console.log(this.$app.$Router);
-    this.$app.$Router.push({ url: item.url });
+    // this.$app.$Router.push({ url: item.url });
   }
 
   render() {
     return (
       <div className="Index">
-        <NavigationBar></NavigationBar>
-        <div className="container">
-          <QuickLaunchBar
-            onGoRoute={(item) => this.handleGoRoute(item)}
-          ></QuickLaunchBar>
-          <div className="content-iframe">
-            <div id="app4"></div>
-          </div>
-        </div>
+        <input
+          className="demo-button"
+          type="button"
+          value="测试"
+          onClick={() => this.handleDemo()}
+        />
       </div>
     );
   }
--- webpack/config/postcss.config.js
+++ webpack/config/postcss.config.js
@@ -3,7 +3,7 @@ module.exports = (api) => {
     // 你可以指定下面提到的所有选项 https://postcss.org/api/#processoptions
     plugins: [
       // PostCSS 插件
-    //   ["postcss-px-to-viewport", { viewportWidth: 375 }],
+      ["postcss-px-to-viewport", { viewportWidth: 375 }],
     ],
   };
 };
